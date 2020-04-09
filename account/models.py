f3007ec24af56fa89a5aab631f35b1896693fe3d
Unknown <unknown@example.com>
2020-04-10 03:59:49 +0430

    models of user added

---
+++ account/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User (models.Model):
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربر'
+
+    user_name = models.CharField(pk = True , max_length = 20)
+    phone_number = models.CharField(max_length = 11)
+    email_address = models.CharField(max_length = 60)
+    name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+    city = models.CharField(default = 'تهران' , max_length = 30)
