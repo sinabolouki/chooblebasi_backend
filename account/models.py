1729a47eeafa9ae660981670246729dd5a76f549
SinaBolouki <sina.bolouki@gmail.com>
2020-04-13 19:00:25 +0430

    some minor changes (Clothe changed to Clothing)

--- account/models.py
+++ account/models.py
@@ -7,18 +7,9 @@ class User(models.Model):
         verbose_name = 'کاربر'
         verbose_name_plural = 'کاربر'
 
-<<<<<<< Updated upstream
-    user_name = models.CharField(primary_key = True , max_length = 20)
-    phone_number = models.CharField(max_length = 11)
-    email_address = models.CharField(max_length = 60)
-    name = models.CharField(max_length = 20)
-    last_name = models.CharField(max_length = 20)
-    city = models.CharField(default = 'تهران' , max_length = 30)
-=======
     user_name = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=11)
     email_address = models.CharField(max_length=60)
     name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     city = models.CharField(default='تهران', max_length=30)
->>>>>>> Stashed changes
--- item/admin.py
+++ item/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from item.models import Clothe
-admin.site.register(Clothe)
+from item.models import Clothing
+admin.site.register(Clothing)
 # Register your models here.
