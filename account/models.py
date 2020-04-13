3e3cb9582a435587a63c9b20a18a863018ac85ea
Unknown <unknown@example.com>
2020-04-13 23:28:02 +0430

    changes on acc

--- account/models.py
+++ account/models.py
@@ -13,3 +13,10 @@ class User(models.Model):
     name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     city = models.CharField(default='تهران', max_length=30)
+    MAN = 1
+    WOMEN = 2
+    kind_gender(
+    (MAN , 'مرد' ),
+    (WOMEN ,'زن')
+    )
+    gender = models.IntegerField(choices=kind_gender)
